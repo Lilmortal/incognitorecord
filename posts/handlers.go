7f64cc1f648b9dbff24b3c621ff8843aba325a31
Jack Tan <Jack.Tan@airnz.co.nz>
2018-08-16 17:06:03 +1200

    Added examples of query param handling

--- posts/handlers.go
+++ posts/handlers.go
@@ -8,5 +8,15 @@ import (
 // HandleRoute handles route
 func HandleRoute(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "rgrhr")
-	fmt.Println(request.URL)
+
+	// thing := strings.TrimPrefix(request.URL.Path, "/posts/")
+
+	id, err := request.URL.Query()["key"]
+
+	if !err || len(id[0]) < 1 {
+		fmt.Fprintf(writer, "Can't find key")
+		return
+	}
+
+		fmt.Fprintf(writer, id[0])
 }
